4eb6e1e53b237b3a012b1c3ffcac8230681c57ec
BalaMunde123 <balamunde123@gmail.com>
2022-11-14 18:14:28 +0530

    Projrct createSuccessFully

---
+++ src/test/java/com/Bikkadit/DemoAll/DemoAllApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Bikkadit.DemoAll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAllApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
